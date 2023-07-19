f55bbc66a1c052766ebb7a5113fe96ef4b8839e9
Alperen M. Urdoğan <89020497+AlperenMertU@users.noreply.github.com>
2023-07-19 23:16:56 +0300

    dealing with conditioning

--- app.js
+++ app.js
@@ -4,16 +4,12 @@ let startBtn = document.querySelector(".start");
 
 const objects = [
   { id: "rock", x: 330, y: 250, w: 25, h: 25, dx: 3, dy: 3, ani: {}, speed: 1, move: false },
-  { id: "scissors", x: 250, y: 40, w: 25, h: 25, dx: 3, dy: 3, ani: {}, speed: 1, move: false },
+  { id: "scissors", x: 220, y: 40, w: 25, h: 25, dx: 3, dy: 3, ani: {}, speed: 1, move: false },
   { id: "paper", x: 120, y: 25, w: 25, h: 25, dx: 3, dy: 3, ani: {}, speed: 1, move: false },
 
-  { id: "rock1", x: 30, y: 250, w: 25, h: 25, dx: 3, dy: 3, ani: {}, speed: 1, move: false },
-  { id: "scissors1", x: 66, y: 80, w: 25, h: 25, dx: 3, dy: 3, ani: {}, speed: 1, move: false },
-  { id: "paper1", x: 12, y: 65, w: 25, h: 25, dx: 3, dy: 3, ani: {}, speed: 1, move: false },
 
-  { id: "rock2", x: 360, y: 120, w: 25, h: 25, dx: 3, dy: 3, ani: {}, speed: 1, move: false },
-  { id: "scissors2", x: 260, y: 340, w: 25, h: 25, dx: 3, dy: 3, ani: {}, speed: 1, move: false },
-  { id: "paper2", x: 550, y: 100, w: 25, h: 25, dx: 3, dy: 3, ani: {}, speed: 1, move: false },
+
+
 ];
 
 
@@ -26,14 +22,46 @@ objects.forEach((data) => {
   div.style.top = `${data.y}px`;
   div.style.width = `${data.w}px`;
   div.style.height = `${data.h}px`;
+  div.style.position = "absolute"
   box.appendChild(div);
 });
 
-objects.forEach((item) => {
-  document.getElementById(`${item.id}`).style.position = "absolute"
-})
 
 
+function kontrolEt() {
+  const karsilastirmalar = [
+    { item1: "paper", item2: "rock", replacement: "paper" },
+    { item1: "paper", item2: "scissors", replacement: "scissors" },
+    { item1: "rock", item2: "paper", replacement: "paper" },
+    { item1: "rock", item2: "scissors", replacement: "rock" },
+    { item1: "scissors", item2: "rock", replacement: "rock" },
+    { item1: "scissors", item2: "paper", replacement: "scissors" },
+
+  ];
+
+  karsilastirmalar.forEach(({ item1, item2, replacement }) => {
+    const div1 = document.getElementById(item1);//rock
+    const div2 = document.getElementById(item2);//paper
+
+    const rect1 = div1.getBoundingClientRect();//rock
+    const rect2 = div2.getBoundingClientRect();//paper
+
+    if (
+      rect1.right >= rect2.left &&
+      rect1.left <= rect2.right &&
+      rect1.bottom >= rect2.top &&
+      rect1.top <= rect2.bottom &&
+      div1.id === item1 &&
+      div2.id === item2
+
+    ) {
+      div1.id = "paper"
+    }
+
+
+
+  });
+}
 
 
 startBtn.addEventListener("click", () => {
@@ -41,7 +69,7 @@ startBtn.addEventListener("click", () => {
     objects.forEach((obj) => {
       obj.ani = requestAnimationFrame(moveObject.bind(null, obj));
       obj.move = true;
-    })
+    });
   } else {
     objects.forEach((obj) => {
       cancelAnimationFrame(obj.ani);
@@ -49,8 +77,6 @@ startBtn.addEventListener("click", () => {
     });
   }
 });
-
-
 
 function moveObject(obj) {
   if (obj.x > 600 - obj.w || obj.x < 0) {
