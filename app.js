000f6c930bf8143231dd017c572c1c818d839bc6
Alperen M. Urdoğan <89020497+AlperenMertU@users.noreply.github.com>
2023-07-21 16:11:30 +0300

    adding readme.md

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 let box = document.querySelector(".box");
 let startBtn = document.querySelector(".start");
 
+function  setRandom() {
+  const min = 10;
+  const max = 320;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let rndNmbr = setRandom();
+let rndNmbr1 = setRandom()
+let rndNmbr2 = setRandom()
+ console.log(rndNmbr1,rndNmbr, rndNmbr2);
 const objects = [
-  { id: "rock", x: 530, y: 150, w: 25, h: 25, dx: 3, dy: 3, ani: {}, speed: 1, move: false },
-  { id: "paper", x: 420, y: 60, w: 25, h: 25, dx: 3, dy: 3, ani: {}, speed: 1, move: false },
-  { id: "rock1", x: 210, y: 150, w: 25, h: 25, dx: 3, dy: 3, ani: {}, speed: 1, move: false },
- // { id: "paper1", x: 210, y: 60, w: 25, h: 25, dx: 3, dy: 3, ani: {}, speed: 1, move: false },
- // { id: "scissors1", x: 10, y: 150, w: 25, h: 25, dx: 3, dy: 3, ani: {}, speed: 1, move: false },
- // { id: "scissors2", x: 410, y: 160, w: 25, h: 25, dx: 3, dy: 3, ani: {}, speed: 1, move: false },
+  { id: "rock", x: rndNmbr, y: rndNmbr + 50, w:25, h: 25, dx: 3, dy: 3, ani: {}, speed: 1, move: false },
+  { id: "paper", x: rndNmbr1, y: rndNmbr1, w: 25, h: 25, dx: 3, dy: 3, ani: {}, speed: 1, move: false },
+  { id: "scissors", x: rndNmbr2, y: rndNmbr2 , w: 25, h: 25, dx: 3, dy: 3, ani: {}, speed: 1, move: false },
 ];
+
+
+
+
+
 
 objects.forEach((data) => {
   console.log(data);
@@ -23,10 +35,160 @@ objects.forEach((data) => {
   box.appendChild(div);
 });
 
-
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
 
 function check() {
 
+  const kirmizi = rock.getBoundingClientRect();
+  const sari = paper.getBoundingClientRect();
+  const mavi = scissors.getBoundingClientRect();
+
+  // kirmizi sariya dönüyor
+  if (
+    kirmizi.right >= sari.left &&
+    kirmizi.left <= sari.right &&
+    kirmizi.bottom >= sari.top &&
+    kirmizi.top <= sari.bottom &&
+    paper.src === "http://127.0.0.1:5500/assets/paper.png" &&
+    rock.src === "http://127.0.0.1:5500/assets/rock.png" 
+
+    //değdikleri zaman resim değişiyyor benim serc kotrolünü yapmam lazım
+   
+
+  ) {   
+     rock.src = "/assets/paper.png"
+  }
+ 
+  //karşılaştığında paper mavi ise rock kırmızı ise = rockı kırmızı yap
+  if (
+    kirmizi.right >= sari.left &&
+    kirmizi.left <= sari.right &&
+    kirmizi.bottom >= sari.top &&
+    kirmizi.top <= sari.bottom && 
+    paper.src == "http://127.0.0.1:5500/assets/scissors.png" &&
+    rock.src == "http://127.0.0.1:5500/assets/rock.png"
+  ) {
+    paper.src = "http://127.0.0.1:5500/assets/rock.png";
+  }
+ 
+  //karşılaştığında rock sarı ise paper mavi ise = rackı mavi yap
+  if (
+    kirmizi.right >= sari.left &&
+    kirmizi.left <= sari.right &&
+    kirmizi.bottom >= sari.top &&
+    kirmizi.top <= sari.bottom && 
+    rock.src == "http://127.0.0.1:5500/assets/paper.png" &&
+    paper.src == "http://127.0.0.1:5500/assets/scissors.png"
+  ) {
+    rock.src = "http://127.0.0.1:5500/assets/scissors.png";
+  }
+
+
+  if (
+    kirmizi.right >= mavi.left &&
+    kirmizi.left <= mavi.right &&
+    kirmizi.bottom >= mavi.top &&
+    kirmizi.top <= mavi.bottom && 
+    rock.src == "http://127.0.0.1:5500/assets/paper.png" &&
+    scissors.src == "http://127.0.0.1:5500/assets/scissors.png"
+  ) {
+    rock.src = "http://127.0.0.1:5500/assets/scissors.png";
+  }
+
+  if (
+    kirmizi.right >= sari.left &&
+    kirmizi.left <= sari.right &&
+    kirmizi.bottom >= sari.top &&
+    kirmizi.top <= sari.bottom && 
+    rock.src == "http://127.0.0.1:5500/assets/scissors.png" &&
+    paper.src == "http://127.0.0.1:5500/assets/paper.png"
+  ) {
+    paper.src = "http://127.0.0.1:5500/assets/scissors.png";
+  }
+/////
+  
+
+  if (
+    mavi.right >= sari.left &&
+    mavi.left <= sari.right &&
+    mavi.bottom >= sari.top &&
+    mavi.top <= sari.bottom && 
+    scissors.src == "http://127.0.0.1:5500/assets/scissors.png" &&
+    paper.src == "http://127.0.0.1:5500/assets/paper.png"
+  ) {
+    paper.src = "http://127.0.0.1:5500/assets/scissors.png";
+  }
+
+
+  ////
+  if (
+    mavi.right >= sari.left &&
+    mavi.left <= sari.right &&
+    mavi.bottom >= sari.top &&
+    mavi.top <= sari.bottom && 
+    scissors.src == "http://127.0.0.1:5500/assets/rock.png" &&
+    paper.src == "http://127.0.0.1:5500/assets/scissors.png"
+  ) {
+    paper.src = "http://127.0.0.1:5500/assets/rock.png";
+  }
+
+  if (
+    mavi.right >= sari.left &&
+    mavi.left <= sari.right &&
+    mavi.bottom >= sari.top &&
+    mavi.top <= sari.bottom && 
+    scissors.src == "http://127.0.0.1:5500/assets/rock.png" &&
+    paper.src == "http://127.0.0.1:5500/assets/paper.png"
+  ) {
+    scissors.src = "http://127.0.0.1:5500/assets/paper.png";
+  }
+  
+  if (
+    kirmizi.right >= mavi.left &&
+    kirmizi.left <= mavi.right &&
+    kirmizi.bottom >= mavi.top &&
+    kirmizi.top <= mavi.bottom && 
+    rock.src == "http://127.0.0.1:5500/assets/rock.png" &&
+    scissors.src == "http://127.0.0.1:5500/assets/scissors.png"
+  ) {
+    scissors.src = "http://127.0.0.1:5500/assets/rock.png";
+  }
+
+  if (
+    kirmizi.right >= mavi.left &&
+    kirmizi.left <= mavi.right &&
+    kirmizi.bottom >= mavi.top &&
+    kirmizi.top <= mavi.bottom && 
+    rock.src == "http://127.0.0.1:5500/assets/rock.png" &&
+    scissors.src == "http://127.0.0.1:5500/assets/paper.png"
+  ) {
+    rock.src = "http://127.0.0.1:5500/assets/paper.png";
+  }
+/*
+  if (
+    // sarı maviye
+    // makas > kağıt
+    sari.right >= mavi.left &&
+    sari.left <= mavi.right &&
+    sari.bottom >= mavi.top &&
+    sari.top <= mavi.bottom
+  ) {
+    paper.style.backgroundColor = "blue";
+  }
+
+  //mavi kırmızıya
+  // taş > makas
+  if (
+    mavi.right >= kirmizi.left &&
+    mavi.left <= kirmizi.right &&
+    mavi.bottom >= kirmizi.top &&
+    mavi.top <= kirmizi.bottom
+  ) {
+    scissors.style.backgroundColor = "red";
+  }
+*/
 
 }
 
@@ -38,6 +200,7 @@ startBtn.addEventListener("click", () => {
     objects.forEach((obj) => {
       obj.ani = requestAnimationFrame(moveObject.bind(null, obj));
       obj.move = true;
+     
     });
   } else {
     objects.forEach((obj) => {
@@ -66,3 +229,18 @@ function moveObject(obj) {
   }
   check();
 }
+
+///////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
